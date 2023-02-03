da76816739865f5fbca6e5a8d5495534a72bac11
Daniel-Mazzilli <danmazzilli@pursuit.org>
2023-02-03 10:33:31 -0500

    small edits

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -10,8 +10,8 @@ export default function Home() {
         <span>Welcome to SNAX!</span>
         <br/>
         An app that allows you to log your favorite snacks, and some of ours too 😉 .{" "} While traversing this app, we have ensured that you will be able to see if whether or{" "}
-        <Link id="hint" to="/snacks/new">
-          d3v$f4v$
+        <Link id="hint" className="type in name" to="/snacks/new">
+          P0i$0n
         </Link>{" "} not your snack is healthy.
         View some of our favorite couch and blanket companions, add your own snacks, come back to update them, and if they have lived on past their expiration date, delete them. 
         <br/>
